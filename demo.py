dcc1b1c49e3eef56cba008530e173b04762e66ce
Tetsu HARUYAMA <tetsu@T-MBP3.local>
2022-09-07 12:45:42 +0900

    from scratch

--- demo.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-import japanize_matplotlib
-
-plt.plot([1, 2, 3, 4])
-plt.xlabel('簡単なグラフ')
-plt.show()
--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 # encoding: utf8
-#from distutils.core import setup
+import glob
 from setuptools import find_packages, setup
-from os import path
 
-with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+additional_files = []
+for filename in glob.iglob('./japanize_matplotlib/**', recursive=True):
+    additional_files.append(filename.replace('./japanize_matplotlib/', ''))
 
-install_requires = ['matplotlib']
 
 setup(name='japanize-matplotlib',
       version='1.1.3',
@@ -14,12 +13,10 @@ setup(name='japanize-matplotlib',
       author='uehara1414',
       author_email='akiya.noface@gmail.com',
       url='https://github.com/uehara1414/japanize-matplotlib',
-      long_description=long_description,
+      long_description=open('README.md').read(),
       long_description_content_type="text/markdown",
       license='MIT License',
       packages=find_packages(),
-      install_requires=install_requires,
+      install_requires=['matplotlib'],
       include_package_data=True,
-      package_data={
-          'japanize_matplotlib': ['fonts/*'],
-      })
+      package_data={'japanize_matplotlib': additional_files})
